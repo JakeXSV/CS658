24e74657b3f3069cb04586e6cfa99643e2d7e9fd
Jake Prather <JakeHP@Zoho.com>
2014-02-21 21:31:09 -0600

    lecture 5/photomailer init

---
+++ PhotoMailer/PhotoMailer/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  PhotoMailer
+//
+//  Created by Jake on 2/21/14.
+//  Copyright (c) 2014 Jake. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
