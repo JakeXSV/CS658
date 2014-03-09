76280653f5a0bb36bfe4507462b7111c16c1601c
Jake Prather <JakeHP@Zoho.com>
2014-03-09 00:12:51 -0600

    finished lecture, fixed audio issues

--- Soundboard/Soundboard/SoundBoardViewController.m
+++ Soundboard/Soundboard/SoundBoardViewController.m
@@ -10,6 +10,7 @@
 
 @interface SoundBoardViewController ()
 @property(nonatomic, strong) NSMutableArray* sounds;
+@property (nonatomic, strong) AVAudioPlayer *player;
 @end
 
 @implementation SoundBoardViewController
@@ -17,45 +18,18 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
     _sounds = [[NSMutableArray alloc]init];
-    NSError* error=nil;
-    AVAudioPlayer* audioPlayer;
-    NSURL* soundUrl;
-    
-    soundUrl = [[NSBundle mainBundle] URLForResource:(@"bears") withExtension:(@"mp3")];
-    audioPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:(soundUrl) error:&error];
-    [audioPlayer prepareToPlay];
-    [audioPlayer play];
-    
-    NSLog(@"%d", [audioPlayer isPlaying]);
-    //[self addSounds];
+    [self addSounds];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 -(void)addSounds{
 
-    NSError* error;
-    AVAudioPlayer* audioPlayer;
-    NSURL* soundUrl;
-    NSString* soundPath;
-    NSString* soundName;
-    Sound* soundBit;
-    
-    soundPath = [[NSBundle mainBundle] pathForResource:(@"afternoondelight") ofType:(@"mp3")];
-    soundUrl = [NSURL fileURLWithPath:(soundPath)];
-    audioPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:(soundUrl) error:&error];
-    soundName = @"POINTLESS?";
-    soundBit = [[Sound alloc]initWithName:(soundName) audioPlayer:(audioPlayer)];
-    [self.sounds addObject:(soundBit)];
-
-    /*
-     [self.sounds addObject:([self createSoundBit:(@"Bears") path:(@"bears")])];
+    [self.sounds addObject:([self createSoundBit:(@"Bears") path:(@"bears")])];
     [self.sounds addObject:([self createSoundBit:(@"I Love Scotch") path:(@"ilovescotch")])];
     [self.sounds addObject:([self createSoundBit:(@"Milk Was a Bad Choice") path:(@"milkwasabadchoice")])];
     [self.sounds addObject:([self createSoundBit:(@"Oh Baxter...") path:(@"ohbaxter")])];
@@ -63,7 +37,6 @@
     [self.sounds addObject:([self createSoundBit:(@"San Diago") path:(@"sandiago")])];
     [self.sounds addObject:([self createSoundBit:(@"Slap You In Public") path:(@"slap")])];
     [self.sounds addObject:([self createSoundBit:(@"Smelly Pirate") path:(@"smellypirate")])];
-    */
 }
 
 -(Sound*)createSoundBit:(NSString*)name path:(NSString*)path{
@@ -96,11 +69,11 @@
 }
 
 -(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
-    /*
+    
     Sound* soundToBePlayed = [self.sounds objectAtIndex:(indexPath.row)];
-    [soundToBePlayed.audioPlayer prepareToPlay];
-    [soundToBePlayed.audioPlayer play];
-     */
+    _player = [soundToBePlayed audioPlayer];
+    [_player play];
+
 }
 
 @end
