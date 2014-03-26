56cb3501f7a6d8b9006fd383383cedd612fe9ea9
Jake Prather <JakeHP@Zoho.com>
2014-03-26 17:48:10 -0500

    assignment 8 - uidatepicker + label

--- TodoList/TodoList/TodoListItemDetailViewController.h
+++ TodoList/TodoList/TodoListItemDetailViewController.h
@@ -14,6 +14,8 @@
 @property(nonatomic, weak) IBOutlet UITextField* titleTextField;
 @property(nonatomic, weak) IBOutlet UITextView* textTextView;
 @property(nonatomic, weak) IBOutlet UISwitch* completedSwitch;
+@property(nonatomic, weak) IBOutlet UIDatePicker* dueDatePicker;
+@property(nonatomic, weak) IBOutlet UILabel* dueDateLabel;
 @property(nonatomic, weak) TodoListItem* item;
 
 -(IBAction)screenTapped;
